4a10915c9808fdd42a88698fb80ddc1ba28bd833
Leonina013 <118098471+Leonina013@users.noreply.github.com>
2023-10-19 03:44:35 +0530

    Update prakruti.py

--- prakruti.py
+++ prakruti.py
@@ -74,16 +74,5 @@ if __name__ == '__main__':
     dominant_dosha = max(dosha_scores, key=dosha_scores.get)
     st.write(f"Your Dominant Dosha: {dominant_dosha}")
 
-    # Generate a plot of the dosha scores
-    doshas = list(dosha_scores.keys())
-    scores = list(dosha_scores.values())
-
-    # Create a bar chart using Matplotlib
-    fig, ax = plt.subplots()
-    ax.bar(doshas, scores)
-    ax.set_xlabel("Dosha")
-    ax.set_ylabel("Score")
-    ax.set_title("Dosha Scores")
-
-    # Display the Matplotlib plot using st.pyplot()
-    st.pyplot(fig)
+    # Create a bar plot for dosha scores using Streamlit
+    st.bar_chart(dosha_scores)
