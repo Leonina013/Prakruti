894c7e51e1a3cbe36a8398f521635d15557f3082
Leonina013 <118098471+Leonina013@users.noreply.github.com>
2023-10-19 11:03:33 +0530

    Update prakruti.py

--- prakruti.py
+++ prakruti.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 
-questions = {
+questions_prakruti = {
     "Body size": ["Slim", "Medium", "Large"],
     "Body weight": ["Low", "Medium", "Overweight"],
     "Chin": ["Thin, angular", "Tapering", "Rounded, double"],
@@ -36,42 +36,102 @@ questions = {
     "Finances": ["Poor, spends on trifles", "Spends money on luxuries", "Rich, good money preserver"]
 }
 
-dosha_scores = {
+questions_vikruti = {
+    "Appearance": ["Thin, bony, emaciated", "Medium, intense", "Large, sluggish"],
+    "Weight": ["Underweight", "Steady", "Overweight"],
+    "Joints": ["Cracking and popping", "Tender", "Swollen"],
+    "Spine": ["Scoliosis tendency", "Kyphosis tendency", "Lordosis tendency, slipped disk"],
+    "Muscles": ["Tremors, tics, spasms", "Tenderness", "Swelling"],
+    "Skin": ["Dark, dry, rough, scaly, liver spots", "Yellow or red, rashes, pimples, acne", "Pale, oily, smooth, swelling"],
+    "Lymph Nodes": ["Skinny", "Tender, inflamed", "Enlarged, congested"],
+    "Veins": ["Prominent, collapsed, empty", "Bruises easily, moderate visibility", "Full, wide, stagnant"],
+    "Eyes": ["Dry, restless, blinking", "Red, burning, hypersensitive to light", "Pale, swollen, sticky, excessive lacrimation"],
+    "Ears": ["Ringing (tinnitus)", "Pain, infections", "Clogged, discharge"],
+    "Nose, Sinuses": ["Dry, crusty", "Red, inflamed", "Congestion"],
+    "Lips": ["Dry, cracked", "Red, inflamed", "Pale, oily"],
+    "Mouth": ["Dry, receding gums", "Red, inflamed and tender gums", "Excessive salivation"],
+    "Teeth": ["Cavities, receding gums, cracked enamel", "Yellow, wasted enamel", "Sweet tooth, strong enamel"],
+    "Tongue": ["Dry, cracked, tremors, dark coating", "Red, inflamed, yellow coating", "Pale, thick white coating"],
+    "Hair": ["Dry, knotted, brittle", "Oily, graying, bald", "Oily, wavy"],
+    "Nails": ["Dry, rough, brittle, cracked, bitten", "Soft, sharp, inflamed", "Pale, thick, oily"],
+    "Appetite": ["Variable, anorexia nervosa", "Strong, unbearable, hypoglycemia", "Low, steady"],
+    "Digestion": ["Irregular, gas and bloating", "Quick, acid indigestion", "Slow, prolonged, indigestion"],
+    "Metabolism": ["Irregular", "Hyperactive", "Slow"],
+    "Thirst": ["Variable", "Strong", "Low"],
+    "Elimination": ["Constipation, dry, hard stools", "Loose stools, diarrhea, burning", "Heavy, oily stools with mucus"],
+    "Energy Level": ["Hyperactive, exhausts quickly", "Intense, exhausts from excessive thinking", "Low, exhaustion due to excess weight"],
+    "Sex Drive": ["Premature orgasm", "Painful sex", "Low libido"],
+    "Liver & Spleen": ["Palpable (double normal size)", "Tender", "Enlarged, fatty degenerative changes"],
+    "Voice": ["Dry, exhausted, explosive, whispering, stuttering", "Sharp, penetrating, metallic", "Deep, hoarse, drum-like"],
+    "Speech": ["Rapid, abrupt, unclear ideas", "Sharp, determined, premeditated", "Slow, monotonous"],
+    "Breathing": ["Nervous, diaphragmatic (sympathetic nervous system)", "Aggressive, intercostals, tight in chest", "Slow, abdominal, apnea tendency"],
+    "Allergies": ["Dry wheezing, breathlessness", "Hives, rashes, urticaria", "Congestion, runny nose"],
+    "Sleep": ["Insomnia, broken", "Difficult entering, insufficient", "Excessive, drowsiness"],
+    "Dreams": ["Plenty, active, fearful", "Fiery, violent", "Watery, romantic"],
+    "Emotions": ["Anxiety, fear, loneliness", "Judgment, criticism, anger, hate, jealousy", "Attachment, greed, depression"],
+    "Intellect": ["Fast but faulty response", "Abrupt but accurate response", "Slow but exact response"],
+    "Memory": ["Recent good, remote poor", "Moderate, distinct", "Slow, remote very good"]
+}
+
+dosha_scores_prakruti = {
     "VATA": 0,
     "PITTA": 0,
     "KAPHA": 0
 }
 
+dosha_scores_vikruti = {
+    "VATA": 0,
+    "PITTA": 0,
+    "KAPHA": 0
+}
 
 if __name__ == '__main__':
-    st.title("Prakruti Constitution Quiz")
+    st.title("Prakruti & Vikruti Constitution Quiz")
 
-    for question, options in questions.items():
-       
+    st.write("### Prakruti Observation")
+    for question, options in questions_prakruti.items():
         st.write(f"**{question}**")
-
-        
         user_answer = st.radio(f"Select the option which fits best for the condition of your {question}", options)
-
         if user_answer:
-           
             if user_answer == options[0]:
-                dosha_scores["VATA"] += 1
+                dosha_scores_prakruti["VATA"] += 1
             elif user_answer == options[1]:
-                dosha_scores["PITTA"] += 1
+                dosha_scores_prakruti["PITTA"] += 1
             elif user_answer == options[2]:
-                dosha_scores["KAPHA"] += 1
-
+                dosha_scores_prakruti["KAPHA"] += 1
             st.success(f'Your answer is: {user_answer}')
 
-    st.write("Your Dosha Scores:")
-    for dosha, score in dosha_scores.items():
+    st.write("### Vikruti Observation")
+    for question, options in questions_vikruti.items():
+        st.write(f"**{question}**")
+        user_answer = st.radio(f"Select the option which fits best for the condition of your {question}", options)
+        if user_answer:
+            if user_answer == options[0]:
+                dosha_scores_vikruti["VATA"] += 1
+            elif user_answer == options[1]:
+                dosha_scores_vikruti["PITTA"] += 1
+            elif user_answer == options[2]:
+                dosha_scores_vikruti["KAPHA"] += 1
+            st.success(f'Your answer is: {user_answer}')
+
+    st.write("### Prakruti Dosha Scores:")
+    for dosha, score in dosha_scores_prakruti.items():
         st.write(f"{dosha}: {score}")
 
- 
-    dominant_dosha = max(dosha_scores, key=dosha_scores.get)
-    st.write(f"Your Dominant Dosha: {dominant_dosha}")
+    st.write("### Vikruti Dosha Scores:")
+    for dosha, score in dosha_scores_vikruti.items():
+        st.write(f"{dosha}: {score}")
 
-    # Create a bar plot for dosha scores
-    dosha_df = pd.DataFrame(list(dosha_scores.items()), columns=["Dosha", "Score"])
-    st.bar_chart(dosha_df.set_index("Dosha"))
+    prakruti_dominant_dosha = max(dosha_scores_prakruti, key=dosha_scores_prakruti.get)
+    vikruti_dominant_dosha = max(dosha_scores_vikruti, key=dosha_scores_vikruti.get)
+
+    st.write(f"### Prakruti Dominant Dosha: {prakruti_dominant_dosha}")
+    st.write(f"### Vikruti Dominant Dosha: {vikruti_dominant_dosha}")
+
+    # Create a bar plot for prakruti dosha scores
+    prakruti_df = pd.DataFrame(list(dosha_scores_prakruti.items()), columns=["Dosha", "Prakruti Score"])
+    st.bar_chart(prakruti_df.set_index("Dosha"))
+
+    # Create a bar plot for vikruti dosha scores
+    vikruti_df = pd.DataFrame(list(dosha_scores_vikruti.items()), columns=["Dosha", "Vikruti Score"])
+    st.bar_chart(vikruti_df.set_index("Dosha"))
