8e1a881a2510feb51e19a7a5aa1d368d25ccf2ca
Leonina013 <118098471+Leonina013@users.noreply.github.com>
2023-11-03 11:39:38 +0530

    Update prakruti.py

--- prakruti.py
+++ prakruti.py
@@ -94,8 +94,7 @@ if __name__ == '__main__':
 
     st.title("Prakruti & Vikruti Constitution Quiz")
 
-    qr_code_url = "https://raw.githubusercontent.com/Leonina013/Prakruti/main/prak.png"  
-    st.image(qr_code_url)
+    st.sidebar.image("https://raw.githubusercontent.com/Leonina013/Prakruti/main/qr_code.png", use_container_width=True)
 
     col1, col2 = st.columns(2)
 
